1503b5a0844d1cd32e4e57620bd1aeb08cf7a805
AndresReyes44 <A01562038@items.mx>
2021-03-20 22:31:42 -0600

    laboratorio 2

---
+++ src/test/java/mx/tec/lab/OtherClassTest.java
@@ -0,0 +1,21 @@
+package mx.tec.lab;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OtherClassTest {
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+		assert ("multiply") != null;
+	}
+
+	@Test
+	public void testMultiply_ExceptionIsThrown() {
+		fail("Not yet implemented");
+		assert ("multiply") != null;
+	}
+
+}
